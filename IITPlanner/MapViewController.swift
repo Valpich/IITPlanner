a2990429414cb3e54192be099af0f16cd78b92fa
Valentin Pichavant <valentinpichavant@Valentins-MacBook-Pro.local>
2016-09-20 21:14:06 -0500

    Beginning of maps integration

---
+++ IITPlanner/MapViewController.swift
@@ -0,0 +1,87 @@
+//
+//  MapViewController.swift
+//  IITPlanner
+//
+//  Created by Valentin Pichavant on 9/20/16.
+//  Copyright © 2016 Valentin Pichavant. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+
+    @IBOutlet var viewMap: GMSMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    /*
+    
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        view = viewMap
+        let myLocation: CLLocation = locationManager.location!
+        viewMap.camera = GMSCameraPosition.camera(withTarget: myLocation.coordinate, zoom: 10.0)
+        viewMap.settings.myLocationButton = true
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = viewMap
+    }*/
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!)
+    {
+        locationManager.stopUpdatingLocation()
+        if ((error) != nil)
+        {
+            print(error)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!)
+    {
+        var locationArray = locations as NSArray
+        var locationObj = locationArray.lastObject as! CLLocation
+        var coord = locationObj.coordinate
+        print(coord.latitude)
+        print(coord.longitude)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
